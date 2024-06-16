5256ae8a38e4802abb9cf2af53af221b78c75bc7
subrata <shuvra.dev9@gmail.com>
2024-06-17 00:27:02 +0600

    Fixed cokkie provider

--- src/components/landingpageItems/LanguageSelect.tsx
+++ src/components/landingpageItems/LanguageSelect.tsx
@@ -4,48 +4,12 @@ import {countries, UserLocation} from "@/constants/Constants";
 import axios from "axios";
 
 export const LanguageSelect = () => {
-    const [location, setLocation] = useState<UserLocation>({lat: null, lng: null});
-    const [country, setCountry] = useState<string | null>(null);
-    const [error, setError] = useState<string | null>(null);
-
-    const getLocation = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position: GeolocationPosition) => {
-                    setLocation({
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude
-                    });
-                },
-                (error: GeolocationPositionError) => {
-                    setError(error.message);
-                }
-            );
-        } else {
-            setError("Geolocation is not supported by this browser.");
-        }
-    };
-
-    const fetchCountry = async (lat: number, lng: number) => {
-        try {
-            const response = await axios.get(`https://api.opencagedata.com/geocode/v1/json`, {
-                params: {
-                    q: `${lat}+${lng}`,
-                    key: 'AIzaSyCs68yEJU3FsR5QVfSVRaJ-tpHwgyg61to'
-                }
-            });
-            const country = response.data.results[0].components.country;
-            setCountry(country);
-        } catch (error) {
-            setError("Error fetching country information");
-        }
-    };
 
     const [selectedFlag, setSelectedFlag] = useState(
-        localStorage.getItem("lang")
+        typeof window !== 'undefined' && localStorage.getItem("lang")
     );
 
-    if (!localStorage.getItem("lang")) {
+    if (typeof window !== 'undefined' && !localStorage.getItem("lang")) {
         localStorage.setItem("lang", "us");
         setSelectedFlag("us");
     }
