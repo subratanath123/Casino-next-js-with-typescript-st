8bce3a3ee36b1c8bebb72659571fe2c009da6e97
subrata <shuvra.dev9@gmail.com>
2024-06-16 21:58:51 +0600

    Fixed cokkie provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Providers from "@/components/Providers";
+import {CookiesProvider} from "next-client-cookies/server";
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
 
     return (
-        <>
+        <CookiesProvider>
             {children}
-        </>
+        </CookiesProvider>
     )
 }
